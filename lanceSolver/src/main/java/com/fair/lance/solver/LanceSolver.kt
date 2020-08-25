029191e71154a66e8002979632bdc0b6b3bb111d
Cerve <59832296+cerver1@users.noreply.github.com>
2020-08-25 19:03:25 -0400

    split test success

--- lanceSolver/src/main/java/com/fair/lance/solver/LanceSolver.kt
+++ lanceSolver/src/main/java/com/fair/lance/solver/LanceSolver.kt
@@ -1,5 +1,6 @@
 package com.fair.lance.solver
 
+import java.lang.Exception
 import kotlin.math.min
 import kotlin.math.pow
 
@@ -9,70 +10,77 @@ class LanceSolver {
         return problem
     }
 
+    /**
     fun stringEquationSolver(entry: String) : String {
 
-        var equation: String = entry
-        var special = searchForSpecial(equation)
-        var oGScope: String
-        var scope: String
-        var firstN: Int
-        var secondN: Int
-        var operator: String
-        var solution = 0
-        var problem: String
-        var parenthesis = trackBrackets(entry)
+    var equation: String = entry
+    var special = searchForSpecial(equation)
+    var oGScope: String
+    var scope: String
+    var firstN: Int
+    var secondN: Int
+    var operator: String
+    var solution = 0
+    var problem: String
+    var parenthesis = trackBrackets(entry)
 
-        // auto solved an equation
-        while (special.isNotEmpty()) {
-            // scopes into the
+    // auto solved an equation
+    while (special.isNotEmpty()) {
+    // scopes into the
 
-            oGScope = sliceParenthesis(equation,parenthesis[0])
-            scope = oGScope
-            println("Scope: $scope")
+    oGScope = sliceParenthesis(equation,parenthesis[0])
+    scope = oGScope
+    println("Scope: $scope")
 
-            while(searchForSpecial(scope).isNotEmpty()){
+    while(searchForSpecial(scope).isNotEmpty()){
 
-                firstN = extractValue(sliceNumber(splitUserInput(scope), searchForSpecial(scope)).first).toInt()
-                println("First Number: $firstN")
-                secondN = extractValue(sliceNumber(splitUserInput(scope), searchForSpecial(scope)).second).toInt()
-                println("Second Number: $secondN")
-                operator = sliceNumber(splitUserInput(scope), searchForSpecial(scope)).third.second
-                println("Operator: $operator")
-                problem = sliceNumber(splitUserInput(scope), searchForSpecial(scope)).third.first
-                println("Problem: $problem")
-                solution = operationAttempt(firstN,secondN, operator)
-                println("Solution: $solution")
-                scope = replaceEquation(problem, solution.toString(), scope)
-                println("Updated Scope: $scope")
-                special = searchForSpecial(scope)
-                println("New Scope Specials: $special")
-            }
+    firstN = extractValue(sliceNumber(splitUserInput(scope), searchForSpecial(scope)).first).toInt()
+    println("First Number: $firstN")
+    secondN = extractValue(sliceNumber(splitUserInput(scope), searchForSpecial(scope)).second).toInt()
+    println("Second Number: $secondN")
+    operator = sliceNumber(splitUserInput(scope), searchForSpecial(scope)).third.second
+    println("Operator: $operator")
+    problem = sliceNumber(splitUserInput(scope), searchForSpecial(scope)).third.first
+    println("Problem: $problem")
+    solution = operationAttempt(firstN,secondN, operator)
+    println("Solution: $solution")
+    scope = replaceEquation(problem, solution.toString(), scope)
+    println("Updated Scope: $scope")
+    special = searchForSpecial(scope)
+    println("New Scope Specials: $special")
+    }
 
-            equation = replaceEquation("($oGScope)", scope, equation)
-            parenthesis = trackBrackets(equation)
-            println("Updated Equation: $equation")
-            special = searchForSpecial(equation)
-            println("New Equation Specials: $special")
-        }
+    equation = replaceEquation("($oGScope)", scope, equation)
+    parenthesis = trackBrackets(equation)
+    println("Updated Equation: $equation")
+    special = searchForSpecial(equation)
+    println("New Equation Specials: $special")
+    }
 
-        return solution.toString()
+    return solution.toString()
 
     }
-    //removes all white space and returning a simple tight list.
+     */
+    // removes all white space and returning a simple tight list.
+    // tested
     fun splitUserInput(userInput: String): MutableList<String> {
 
         val trim = userInput.replace("\\s".toRegex(), "")
         return trim.split("").toMutableList()
     }
-    //finds the special operators within the list
-    private fun searchForSpecial(equation: String) : MutableList<Int> {
-        val specialList = arrayListOf("^","*","/","+","-")
-        val specialParenthesis = arrayListOf('^','*','/','+','-')
+
+    // finds the special operators within the list
+    // tested
+    fun searchForSpecial(equation: String): MutableList<Int> {
+        val specialList = arrayListOf("^", "*", "/", "+", "-")
+        val specialParenthesis = arrayListOf('^', '*', '/', '+', '-')
         val confirmedSpecial = mutableListOf<Int>()
 
-        val result = equation.toCharArray().intersect(specialParenthesis).map { x -> min(equation.count {it == x}, specialParenthesis.count {it == x}) }.sum()
+        val result = equation.toCharArray().intersect(specialParenthesis)
+            .map { x -> min(equation.count { it == x }, specialParenthesis.count { it == x }) }
+            .sum()
 
-        return if(result == 0) {
+        return if (result == 0) {
             confirmedSpecial
         } else {
             equation.forEachIndexed { index, c ->
@@ -84,105 +92,146 @@ class LanceSolver {
         }
 
     }
-    //separates by parenthesis
-    private fun sliceParenthesis(userEquation: String, special: Pair<Int, Int> ): String {
+
+    // separates by parenthesis
+    // tested
+    fun sliceParenthesis(userEquation: String, special: Pair<Int, Int>): String {
 
         val startSlice = special.first
         val endSlice = special.second
 
-        return userEquation.slice(startSlice+1 until endSlice)
+        return userEquation.slice(startSlice + 1 until endSlice)
     }
-    //pulls the first preceding and succeeding values of
-    private fun sliceNumber(userEquation: MutableList<String>, special: MutableList<Int>): Triple<MutableList<String>, MutableList<String>, Pair<String, String>>{
-        var firstNumber = mutableListOf<String>()
-        var secondNumber = mutableListOf<String>()
-        val problemScope : String
-        return if (special.size > 1) {
-            firstNumber = userEquation.slice(0 until special.first()).toMutableList()
-            secondNumber = userEquation.slice(special[0] +1 until special[1]).toMutableList()
-            problemScope = extractValue(userEquation.slice(0 until special[1]).toMutableList())
-            Triple(firstNumber,secondNumber,Pair(problemScope,userEquation[special.first()]))
 
+    // returns the first preceding and succeeding values of a special value
+    // tested
+    fun sliceNumber(
+        userEquation: MutableList<String>,
+        special: MutableList<Int>
+    ): Triple<Int, Int, String> {
+        val firstNumber: Int
+        val secondNumber: Int
+        //val equationScope: String
+
+        return if (special.size > 1) {
+            firstNumber =
+                userEquation.slice(0 until special.first()).joinToString(separator = "").toInt()
+            secondNumber = userEquation.slice(special.first() + 1 until special[1])
+                .joinToString(separator = "").toInt()
+            Triple(firstNumber, secondNumber, userEquation[special.first()])
         } else {
             firstNumber = userEquation.slice(0 until special.first()).toMutableList()
-            secondNumber = userEquation.slice(special.first() + 1 until userEquation.size).toMutableList()
-            problemScope = extractValue(userEquation.slice(0 until userEquation.size).toMutableList())
-            Triple(firstNumber,secondNumber,Pair(problemScope,userEquation[special.first()]))
-
+                .joinToString(separator = "").toInt()
+            secondNumber = userEquation.slice(special.first() + 1 until userEquation.size)
+                .joinToString(separator = "").toInt()
+            Triple(firstNumber, secondNumber, userEquation[special.first()])
         }
+
     }
+
+    /**
     //turns a list of string numbers into a an single int value
-    private fun extractValue(listedNumber:MutableList<String>): String {
-        val number = StringBuilder()
-        listedNumber.forEach {
-            number.append(it)
-        }
 
-        return number.toString()
+    private fun extractValue(listedNumber:MutableList<String>): String {
+    val number = StringBuilder()
+    listedNumber.forEach {
+    number.append(it)
     }
+
+    return number.toString()
+    }
+     */
     //run calculations on first and second number based on the received operator
-    private fun operationAttempt(firstN: Int, secondN: Int, operator: String): Int{
-        return when(operator) {
-            "^" -> firstN.toFloat().pow(secondN).toInt()
-            "*" -> firstN.times(secondN)
-            "/" -> firstN.div(secondN)
-            "+" -> firstN.plus(secondN)
-            "-" -> firstN.minus(secondN)
-            else -> 0
+    // tested
+    fun operationAttempt(firstN: Int, secondN: Int, operator: String): String {
+        return when (operator) {
+            "^" -> firstN.toDouble().pow(secondN).toString()
+            "*" -> firstN.times(secondN).toString()
+            "/" -> firstN.div(secondN.toDouble()).toString()
+            "+" -> firstN.plus(secondN).toString()
+            "-" -> firstN.minus(secondN).toString()
+            else -> "null"
         }
     }
-    private fun replaceEquation(problem: String, solution: String, equation: String): String {
+
+    // replace the problem snippet with the solution within the original problem to return a new equation
+    // tested
+    fun replaceEquation(problem: String, solution: String, equation: String): String {
         var newEquation = equation.replace(problem, solution)
-        when(endPointMultiplication(newEquation, solution)){
+        when (endPointMultiplication(newEquation)) {
             "start" -> newEquation = equation.replace(problem, "*$solution")
             "end" -> newEquation = equation.replace(problem, "$solution*")
             "null" -> newEquation
         }
         return newEquation
     }
-    private fun trackBrackets(string: String): MutableList<Pair<Int,Int>> {
+
+    // for extracting the correct order of brackets within a list
+    // tested
+    fun trackBrackets(string: String): MutableList<Pair<Int, Int>> {
 
         val stack = mutableListOf<Int>()
-        val groupStack = mutableListOf<Pair<Int,Int>>()
+        val groupStack = mutableListOf<Pair<Int, Int>>()
         string.forEachIndexed { index, c ->
 
-            when(c){
+            when (c) {
                 '(' -> stack.add(index)
                 ')' -> {
                     groupStack.add(Pair(stack.last(), index))
-                    stack.removeAt(stack.size -1)
+                    stack.removeAt(stack.size - 1)
                 }
             }
         }
 
         return groupStack
     }
-    private fun endPointMultiplication(equation: String, solution: String): String {
-        val solTrackingStart = equation.indexOf(solution)
-        val solTrackingEnd = equation.reversed().indexOf((solution).reversed())
-        val solStart: Char
-        val solEnd : Char
 
-        solStart = try {
-            equation[solTrackingStart - 1]
-        } catch (e: IndexOutOfBoundsException) {
-            'e'
+    fun endPointMultiplication(equation: String): String {
+
+        var parenthesis = trackBrackets(equation)
+        val specialList = arrayListOf('^', '*', '/', '+', '-')
+        val stringNumbers = arrayListOf('1','2','3','4','5','6','7','8','9','0')
+        val newEquation = StringBuilder(equation)
+
+        parenthesis.forEach { bracket ->
+            println(parenthesis)
+
+            val prefix = (bracket.first)
+            val precede = try {
+                newEquation[prefix - 1]
+            } catch (e: IndexOutOfBoundsException) {
+                'n'
+            }
+
+            if (!specialList.contains(precede) && stringNumbers.contains(precede)) {
+                newEquation.insert(prefix, "*")
+            }
+            parenthesis = trackBrackets(newEquation.toString())
+            println(parenthesis)
         }
 
-        solEnd = try {
-            equation[equation.length - solTrackingEnd]
-        } catch (e: IndexOutOfBoundsException) {
-            'e'
+        parenthesis = trackBrackets(newEquation.toString())
+
+        parenthesis.forEach { bracket ->
+
+            val suffix = (bracket.second + 1)
+            val succeed = try {
+                newEquation[suffix]
+            } catch (e: IndexOutOfBoundsException) {
+                'n'
+            }
+
+            if (!specialList.contains(succeed) && stringNumbers.contains(succeed)) {
+                newEquation.insert(suffix, "*")
+            }
+            parenthesis = trackBrackets(newEquation.toString())
         }
 
-        println(solStart)
-        println(solEnd)
-        return when {
-            solStart == ')' -> "start"
-            solEnd == '(' -> "end"
-            else -> "null"
-        }
 
+            //parenthesis = trackBrackets(newEquation.toString())
+
+
+        return newEquation.toString()
 
     }
 
--- lanceSolver/src/test/java/com/fair/lance/solver/LanceSolverTest.kt
+++ lanceSolver/src/test/java/com/fair/lance/solver/LanceSolverTest.kt
@@ -23,8 +23,95 @@ class LanceSolverTest {
 //    }
 
     @Test
-    fun `split string into mutable list without any empty spaces_is true`(){
+    fun `split string into mutable list without any empty spaces_is true`() {
         val result = main.splitUserInput("( 1 + 2)")
         assertThat(result).doesNotContain(" ")
+    }
+
+    @Test
+    fun `search string for special values is return a list of specials`() {
+        val result = main.searchForSpecial("0*+-/")
+        assertThat(result).containsExactlyElementsIn(mutableListOf(1,2,3,4)) //isEqualTo([0,1,2,3])
+    }
+
+    @Test
+    fun `remove the first occurrence of a parenthesis`() {
+        val result = main.sliceParenthesis("(1+2*(2-4)*1)",Pair(5,9))
+        assertThat(result).isEqualTo("2-4")
+    }
+
+    @Test
+    fun `returns the preceding and succeeding numbers surrounding a single special value`() {
+        val result = main.sliceNumber(mutableListOf("1","3","+","5","2"), mutableListOf(2))
+        assertThat(result).isEqualTo(Triple(13,52,"+"))
+    }
+
+    @Test
+    fun `returns the preceding and succeeding numbers surrounding a multiple special values`() {
+        val result = main.sliceNumber(mutableListOf("1","+","1","2","-","3"), mutableListOf(1,4))
+        assertThat(result).isEqualTo(Triple(1,12,"+"))
+    }
+
+    @Test
+    fun `returns the first number and second number added`() {
+        val result = main.operationAttempt(1, 12, "+")
+        assertThat(result).isEqualTo("13")
+    }
+
+    @Test
+    fun `returns the first number and second number multiplied`() {
+        val result = main.operationAttempt(1, 12, "*")
+        assertThat(result).isEqualTo("12")
+    }
+
+    @Test
+    fun `returns the first number and second number divided`() {
+        val result = main.operationAttempt(1, 12, "/")
+        assertThat(result).isEqualTo("0.08333333333333333")
+    }
+    @Test
+    fun `returns the first number and second number raised`() {
+        val result = main.operationAttempt(1, 12, "^")
+        assertThat(result).isEqualTo("1.0")
+    }
+
+    @Test
+    fun `returns the first number and second number subtracted`() {
+        val result = main.operationAttempt(1, 12, "-")
+        assertThat(result).isEqualTo("-11")
+    }
+
+    @Test
+    fun `return a new equation after replacing the problem snippet with a solution`() {
+        val result = main.replaceEquation("1+3","4", "(1*(1+3))")
+        assertThat(result).isEqualTo("(1*(4))")
+    }
+
+    @Test
+    fun `return all positions of parenthesis within your list in`() {
+        val result = main.trackBrackets("(1(2)3)()")
+        assertThat(result).isEqualTo(mutableListOf(Pair(2,4),Pair(0,6),Pair(7,8)))
+    }
+
+    @Test
+    fun `insert multiplication sign if a special value isn't present between a ) or ( and a number`() {
+        val result= main.endPointMultiplication("(1(1+2)4)")
+        assertThat(result).isEqualTo("(1*(1+2)*4)")
+    }
+
+
+    // change name
+    @Test
+    fun `insert multiplication sign if a special value isn't present between a ) or this ( and a number`() {
+        val result= main.endPointMultiplication("(1+(1+2)4)")
+        assertThat(result).isEqualTo("(1+(1+2)*4)")
+    }
+
+
+    // change name
+    @Test
+    fun `insert multiplication sign if a special value isn't present between a ) or and ( and a number`() {
+        val result= main.endPointMultiplication("(1+3(1+2)(4+2)1)")
+        assertThat(result).isEqualTo("(1+3*(1+2)*(4+2)*1)")
     }
 }
